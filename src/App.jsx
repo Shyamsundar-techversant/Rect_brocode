f4a065b024c0fc661bcdbe847f08a2ecd7cd0bae
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-03-16 12:49:08 +0530

    rendering list part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import List from './List.jsx';
+    const fruits = [{id :1,name : "apple",calories : "s"},
+                    {id:2,name : "orange",calories:45},
+                    {id:3,name : "banana",calories:345}
+                ];
+    const vegetables = [{id :6,name : "potato",calories : 115},
+                    {id:7,name : "celery",calories:15},
+                    {id:8,name : "carrots",calories:25}
+                ];
+
+
 function App() {
   return(
     <>
-      <List />
+      {fruits.length > 0 ?<List items = {fruits} category = "Fruits" /> : null}
+      {/* Without ternary operator -- using logical AND  */}
+      {vegetables.length > 0 && <List items = {vegetables} category = "Vegetables" />}
     </>
   )
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,12 +1,23 @@
-function List(){
-    const fruits = [{id :1,name : "apple",calories : 95},
-                    {id:2,name : "orange",calories:45},
-                    {id:3,name : "banana",calories:345}
-                ];
-    // fruits.sort((a,b)=>a.name.localeCompare(b.name)); //Sort alphabetic
-    fruits.sort((a,b)=>a.calories - b.calories);
-    const lowCalFruits = fruits.filter(fruit =>fruit.calories <100);
-    const listItmes = lowCalFruits.map(fruit =><li key={fruit.id}>{fruit.name} : {fruit.calories}</li>)
-    return(<ul>{listItmes}</ul>);
+import PropTypes from "prop-types";
+function List(props){
+    const category = props.category;
+    const itemList = props.items;
+    const listItmes = itemList.map(item => <li key = {item.id}>
+                                            {item.name}: {item.calories}
+                                            </li>
+                                )
+    return(<>
+        <h3 className="list-category">{category}</h3>
+        <ul className="list-items">{listItmes}</ul>        
+    </>);
 }
+    List.propTypes = {
+        category : PropTypes.string,
+        items : PropTypes.arrayOf(PropTypes.shape({id : PropTypes.number,
+                                                    name : PropTypes.string,
+                                                    calories : PropTypes.number,
+                                                    }
+        )),
+    }
+console.log(List.propTypes);
 export default List;
