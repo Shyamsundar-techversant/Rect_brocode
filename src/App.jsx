b85864ade1dd78596885f91f1b76160129012fbe
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-03-16 17:21:10 +0530

    click event part 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // click event - An interaction when a user clicks on a specific element.
 //   we can respond to clicks by passing a callback to the onClick event handler.
 
-import Button from './Button.jsx'
+import ProfilePicture from './ProfilePicutre.jsx';
 function App() {
   return(
-    <Button/>
+    <ProfilePicture/>
   )
 }
 
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,17 +1,7 @@
 function Button(){
-    let count = 0;
-    const handleClick = (name) => {
-        if(count < 3){
-            count++ ;
-            console.log(`${name} you clicked me ${count} times`);      
-        }
-        else{
-            console.log(`${name} stop clicking me...`)
-        }
-    }
-
+    const handleClick = (e) =>  e.target.textContent = "OUCH";
     return(
-        <button onClick={() => handleClick("BRO")}>Click me</button>
+        <button onClick={(e) => handleClick(e)}>Click me</button>
     )
 }
 export default Button;
