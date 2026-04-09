bc0da50233490ef110674dd30e32fa3106a4b78b
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-04-09 15:39:15 +0530

    useRef part  3

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -12,22 +12,22 @@
 import React , {useState,useEffect,useRef} from "react";
 
 function MyComponent(){
-    const ref = useRef(0);
+    const inputRef = useRef(null);
 
     useEffect(() => {
        console.log("COMPONENT RENDERED"); 
     });
 
     function handleClick(){
-        ref.current ++ ;
-        console.log(ref.current);
+        inputRef.current.focus();
+        inputRef.current.style.backgroundColor = "yellow";
     }
     return(
         <div>
             <button onClick={handleClick}>
                 Click me!
             </button>
-            
+            <input ref={inputRef}></input>
         </div>
 
     )
