7313e36c6d01948f4889cf6e24489b09cdd07739
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-04-09 15:22:07 +0530

    useRef part 1 : Rendering without useRef()

---
+++ src/MyComponent.jsx
@@ -0,0 +1,30 @@
+/* 
+    // useState() - Re-renders the component when the state value changes
+
+    // useRef() - "use Reference" Does not cause re-renders when its value changes.
+                    when we want a component to "remember" some information but
+                    we don't want that information to trigger new renders.
+
+    // 1. Accessing/Interacting with DOM elements.
+    // 2. Handling Focus, Animations and Transitions.
+    // 3. Managing Timers and Intervals.
+*/
+import React , {useState,useEffect,useRef} from "react";
+
+function MyComponent(){
+    let [number,setNumber] = useState(0);
+
+    useEffect(() => {
+       console.log("COMPONENT RENDERED"); 
+    });
+
+    function handleClick(){
+        setNumber(n => n+1);
+    }
+    return(
+        <button onClick={handleClick}>
+            Click me!
+        </button>
+    )
+}
+export default MyComponent;
