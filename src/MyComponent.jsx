a6101c809f689365b86f260dff18df5229701824
Shyamsundar-techversant <shyamsundar.ps@techversantinfotech.com>
2026-04-09 15:32:56 +0530

    useRef part  2

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -12,19 +12,24 @@
 import React , {useState,useEffect,useRef} from "react";
 
 function MyComponent(){
-    let [number,setNumber] = useState(0);
+    const ref = useRef(0);
 
     useEffect(() => {
        console.log("COMPONENT RENDERED"); 
     });
 
     function handleClick(){
-        setNumber(n => n+1);
+        ref.current ++ ;
+        console.log(ref.current);
     }
     return(
-        <button onClick={handleClick}>
-            Click me!
-        </button>
+        <div>
+            <button onClick={handleClick}>
+                Click me!
+            </button>
+            
+        </div>
+
     )
 }
 export default MyComponent;
